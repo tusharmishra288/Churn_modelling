069e824f4c8b479ab003f4be01bdd21cc10d7fcc
Tushar Mishra <tusharmishra288@gmail.com>
2020-07-22 20:06:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import jsonify
+import numpy as np
+import joblib
+app = Flask(__name__)
+
+@app.route('/')
+def Home():
+    return render_template('index.html')
+
+@app.route("/predict", methods=['POST'])
+def predict():
+        to_predict_list = request.form.to_dict() 
+        to_predict_list = list(to_predict_list.values()) 
+        to_predict_list = list(map(float, to_predict_list)) 
+        to_predict = np.array(to_predict_list).reshape(1, 17) 
+        model = joblib.load('project_f') 
+        prediction = model.predict(to_predict) 
+        if prediction==0:
+            return render_template('index.html',prediction_text="Customer will not be churned")
+        else:
+            return render_template('index.html',prediction_text="Customer will be churned")
+      
+
+if __name__=="__main__":
+    app.run(debug=True)            
